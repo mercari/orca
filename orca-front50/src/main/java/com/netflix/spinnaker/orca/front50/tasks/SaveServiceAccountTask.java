d2cde8f41ab852c9c3a3f7af4a719a648825e091
Nathan Essex <nathan@essex.id.au>
2022-07-25 18:58:50 +0900

    Add missing import for Value

--- orca-front50/src/main/java/com/netflix/spinnaker/orca/front50/tasks/SaveServiceAccountTask.java
+++ orca-front50/src/main/java/com/netflix/spinnaker/orca/front50/tasks/SaveServiceAccountTask.java
@@ -41,6 +41,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import retrofit.client.Response;
